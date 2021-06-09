af91cecc20d2c772a800f71690386a97c6b23792
Ramon_kits <Ramon_kits@hotmail.nl>
2021-06-09 14:06:01 +0200

    component locaties georganiseerd

--- omega-share/src/app/app.module.ts
+++ omega-share/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
