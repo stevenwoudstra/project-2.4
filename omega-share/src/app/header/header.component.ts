f14b11e6ebf02dcc21552b96f1bbc23b5f6d2b22
Ramon_kits <Ramon_kits@hotmail.nl>
2021-06-28 10:59:43 +0200

    upload page en begin profile in header

--- omega-share/src/app/header/header.component.ts
+++ omega-share/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
+import { DataSharingService } from '../_services/data-sharing.service';
 import { TokenStorageService } from './../_services/token-storage.service';
 
 @Component({
@@ -10,14 +11,24 @@ import { TokenStorageService } from './../_services/token-storage.service';
 export class HeaderComponent implements OnInit {
   isLoggedIn = false;
 
-  constructor(private tokenStorageService: TokenStorageService) { }
+  username: any;
+  profilePicture: string = "assets/placeholder pictures/stock_profile_picture.png";
 
+  constructor(private tokenStorageService: TokenStorageService, private dataSharingService: DataSharingService) { }
+  
   ngOnInit(): void {
-    this.isLoggedIn = !!this.tokenStorageService.getToken();
+    
+        this.dataSharingService.isUserLoggedIn.subscribe(value => {
+          this.isLoggedIn = value;
+          const user = this.tokenStorageService.getUser();
+          this.username = user.username;
+
+        });
+    // this.isLoggedIn = !!this.tokenStorageService.getToken();
   }
 
   navbarClick() {
-    if(window.matchMedia("(max-width: 575px)").matches) {
+    if (window.matchMedia("(max-width: 575px)").matches) {
       document.getElementById("navbar-btn")?.click();
     }
   }
--- omega-share/src/app/pages/upload/upload.component.ts
+++ omega-share/src/app/pages/upload/upload.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { GlobalConstants } from 'src/app/global-constants';
 
 @Component({
   selector: 'app-upload',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
-  constructor() { }
+  selectedFile: File | null = null;
 
+  url : String = "";
+
+  constructor(private http: HttpClient) { }
+  
   ngOnInit(): void {
+  }
+  
+  onFileSelected(event: any): void {
+
+    
+    document.getElementById("upload-icon")?.remove();
+    let fileType = event.target!.files[0].name.split(".").pop().toUpperCase();
+    let previewIcon: HTMLElement = document.getElementById("preview-icon")!;
+    if(previewIcon.children[2]) previewIcon.children[2].remove();
+    if(GlobalConstants.viewableFileTypes.includes(fileType)) {      
+      previewIcon.firstElementChild!.setAttribute("src", "assets/icons/file_types/" + fileType + ".svg");
+    } else {
+      let typeLabel = document.createElement("label");
+      typeLabel.innerHTML = "." + fileType;
+      typeLabel.style.fontSize = ((3 / fileType.length) > 1 ? 1 : 3 / fileType.length) + "rem";
+      typeLabel.style.transform = "translateY(-70px)";
+      previewIcon.append(typeLabel);
+      previewIcon.firstElementChild!.setAttribute("src", "assets/icons/file_types/unknown.svg");
+    }
+    var displayName = event.target!.files[0].name
+    if(event.target!.files[0].name.length > 16) displayName = displayName.substring(0, 16) + "...";
+    previewIcon.children[1].innerHTML = displayName;
+    previewIcon.style.height = "100px";
+    previewIcon.classList.toggle("fileAppear");
+    previewIcon.classList.toggle("fileAppear2");
+
+    // this.selectedFile = event.target.files[0];
+    // const fd = new FormData();
+    // fd.append('image', this.selectedFile!, this.selectedFile!.name);
+    // this.http.post('omegashare.test', fd).subscribe(res => {
+    //   console.log(res);
+    // })
+
+
+    // if(event.target.files) {
+    //   let reader = new FileReader();
+    //   reader.readAsDataURL(event.target.files[0]);
+    //   reader.onload = (event:any) => {
+    //     this.url = event.target.result;
+    //   }
+    // }
+
   }
 
 }
